1ac3b7c8964223aa7430c0f768a984d75ed1a322
KenDev2105 <195981080+KenDev2105@users.noreply.github.com>
2025-10-14 20:13:48 -0600

    Clase 1 Post-Laboratorio (Funciones)

---
+++ Semana 9/Clase 1/C1S9.js
@@ -0,0 +1,18 @@
+/* Funciones de flecha*/
+// Calcular el cuadrado de un numero
+ 
+const cuadrado = (num) => num * num;
+const cuadradoRes = cuadrado(10)
+console.log(cuadradoRes)
+
+// Calcular promedio de 3 numeros
+
+const promedio = (a, b, c) => (a + b + c)/3;
+const promRes = promedio(10,6,20)
+console.log(promRes)
+
+// Calcular el area de un triangulo
+
+const triangulo = (base, altura) => (base*altura)/2;
+const area = triangulo(10,30)
+console.log(area)
